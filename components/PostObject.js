45ec1f9a661172f96aa8c0a97bcb2a44b181ff9f
Omer Kandemir <farukkandemir585@gmail.com>
2023-04-02 00:12:44 -0400

    fixed post objects

--- components/PostObject.js
+++ components/PostObject.js
@@ -49,44 +49,24 @@ const PostObject = ({PostObject}) => {
 
 const Section = styled.section`
 margin: 1vw 0;
-display: flex;
-justify-content: center;
-align-items: center;
 width: 100%;
-// height: 30vw;
-// background-color: skyblue;
 border-radius: 1vw;
 border: 0.1vw solid gainsboro;
 box-shadow: 0.2vw 0.2vw 0.2vw gainsboro;
-// background: linear-gradient(to bottom, #f5f5f5, white);
-
-
 &:hover{
   box-shadow: 0.1vw 0.1vw 0.1vw gainsboro;
 }
 `
 const Container = styled.div`
-display: flex;
-// background-color: moccasin;
-// width: 74.5vw;
-width: 100%;
-// height: 28vw;
-// margin: 1vw 0;
-flex-direction: column;
 `
 const Header =  styled.div`
 display: flex;
-// width: 100%;
 border-radius: 1vw 1vw 0vw 0vw;
 height: 4vw;
-
 align-items: center;
 justify-content: space-between;
-// background-color: orange;
 font-size: 3vw;
 font-weight: 900;
-// border-bottom: 0.1vw solid gainsboro;
-// background: linear-gradient(to right, #FE5F55, #5B618A);
 background-color: #FE5F55;
 filter: opacity(0.9);
 color: white;
@@ -94,17 +74,11 @@ color: white;
   filter: opacity(1);
   transition: ease 1s;
 }
-
 `
-
-
 const RapperNameLeft = styled.div`
 display: flex;
-width: 25%;
-height: 2vw;
-color: white;
-// height: 100%;
-justify-content: center;
+margin-left: 1vw;
+width: 45%;
 align-items: center;
 font-size: 2vw;
 font-weight: 900;
@@ -112,18 +86,14 @@ img{
   width: 2vw;
   height: 2vw;
   margin-right: 0.5vw;
-  // border: 0.2vw solid #5B618A;
-  border-radius: 100%;
 }
 `
 const RapperNameRight = styled.div`
 display: flex;
-width: 25%;
+width: 45%;
 height: 2vw;
-color: white;
-// height: 100%;
-
-justify-content: center;
+justify-content: right;
+margin-right: 1vw;
 align-items: center;
 font-size: 2vw;
 font-weight: 900;
@@ -131,66 +101,38 @@ img{
   width: 2vw;
   height: 2vw;
   margin-right: 0.5vw;
-  // border: 0.2vw solid #5B618A;
-  border-radius: 100%;
 }
 `
-
 const VideoDiv = styled.div`
-display: flex;
-width: 100%;
 height: 34vw;
-margin: auto auto;
-background-color: black;
-justify-content: center;
-align-items: center;
-// margin: 1vw 0;
-`
-const StyledFaPlay = styled(FaPlay)`
-  color: white;
-  font-size: 8vw;
 `
 const BottomBar = styled.div`
 display: flex;
-width: 100%;
 height: 4vw;
 align-items: center;
 justify-content: space-between;
-// background-color: orange;
-// border-top: 0.1vw solid gainsboro;
 `
 const BottomLeft = styled.div`
 display: flex;
-// width: 50%;
-height: 100%;
-// background-color: greenyellow;
 margin-left: 1vw;
 justify-content: center;
 align-items: center;
 font-size: 1vw;
 background-color: #EBEBEB;
 height: 2.5vw;
-width: 12vw;
+padding-left: 1vw;
+padding-right: 1vw;
 border-radius: 0.5vw;
-// border: 0.1vw solid gainsboro;
-// box-shadow: 0.1vw 0.1vw 0.5vw gainsboro;
-color: black;
 `
 const BottomRight = styled.div`
 display: flex;
-// width: 50%;
 height: 2.5vw;
 background-color: #EBEBEB;
-// border-radius: 2vw 0vw 0vw 2vw;
 margin-right: 1vw;
-justify-content: center;
 border-radius: 0.5vw;
 align-items: center;
-// border: 0.1vw solid gainsboro;
-// box-shadow: 0.1vw 0.1vw 0.5vw gainsboro;
 `
 const ThreeDots = styled(BsThreeDotsVertical)`
-  color: black;
   font-size: 1.5vw;
   margin-left: 2vw;
   margin-right: 0.5vw;
@@ -201,7 +143,6 @@ const ThreeDots = styled(BsThreeDotsVertical)`
   }
 `
 const Comment = styled(TfiCommentAlt)`
-color: black;
 font-size: 1.5vw;
 margin-left: 2vw;
 
@@ -211,10 +152,8 @@ margin-left: 2vw;
 }
 `
 const Downvote = styled(BiDownvote)`
-color: black;
 font-size: 1.5vw;
 margin-left: 2vw;
-
 
 &:hover{
   transform: scale(1.01);
@@ -224,7 +163,6 @@ margin-left: 2vw;
 }
 `
 const Upvote = styled(BiUpvote)`
-color: black;
 font-size: 1.5vw;
 margin-left: 1vw;
 
@@ -235,9 +173,7 @@ margin-left: 1vw;
 }
 `
 const VoteCount = styled.div`
-display: flex;
 font-size: 1vw;
-// font-weight: 900;
 margin-left: 0.25vw;
 `
 export default PostObject
