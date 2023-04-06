9046f441f5936b13fb07a2d75bb4b2207c16beb1
Omar R <omarhishamk@gmail.com>
2023-04-06 14:26:47 -0400

    set up how a user object will be like

---
+++ functionalities/userFunctions.js
@@ -0,0 +1,18 @@
+
+
+/*
+const user = {
+    username: "",
+    full_name: "",
+    profile_image: "",
+    following: [],
+    followers: [],
+    saved_posts: [],
+
+    // stats
+    top_posted_rapper: "",
+    post_numbers: "",
+    total_upvotes: "",
+    total_downvotes:""
+}
+*/
